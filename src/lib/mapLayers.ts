cc6ee55c7b6b56f41ead2d2d9d55b26181c69396
Anastasis-M <77619714+Anastasis-M@users.noreply.github.com>
2026-09-17 17:39:15 +0300

    fix(map): point spatial data at the open S3 endpoint

--- src/lib/mapLayers.ts
+++ src/lib/mapLayers.ts
@@ -13,7 +13,7 @@ import ImageTileSource, {
 } from "ol/source/ImageTile.js";
 import XYZ from "ol/source/XYZ.js";
 
-const SPATIAL_CDN = "https://map-tiles.open-meteo.com/data_spatial";
+const SPATIAL_CDN = "https://openmeteo.s3.amazonaws.com/data_spatial";
 
 export type MapLayerKind = "precipitation";
 
